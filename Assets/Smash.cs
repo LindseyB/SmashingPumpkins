450985ba7f0f289d9ca0c7e046d02f43fd00e82b
Lindsey Bieda <hazardous.narayan@gmail.com>
2015-10-17 19:30:25 -0400

    Initial commit.

---
+++ Assets/Smash.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Smash : MonoBehaviour {
+    [SerializeField] private Text scoreText;
+
+    private bool smashed = false;
+    private static int score = 0;
+
+	void Start () {
+        scoreText.text = score.ToString();
+    }
+	
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            if (smashed) {
+                Application.LoadLevel(0);
+                smashed = false;
+            } else {
+                smashed = true;
+                foreach (Rigidbody rigidbody in gameObject.GetComponentsInChildren<Rigidbody>()) {
+                    rigidbody.isKinematic = false;
+                }
+                score++;
+                scoreText.text = score.ToString();
+            }
+        }
+	}
+}
